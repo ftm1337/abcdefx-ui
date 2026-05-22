b5466acfbda177d4d6301d5fc3bdbe037666f471
ftm1337 <84405345+ftm1337@users.noreply.github.com>
2026-05-22 02:12:57 -0400

    Update tokenlist: f.g -> f.e.n

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -4,5 +4,6 @@ export const DEFAULT_TOKEN_LIST_URL = ''
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   //'https://raw.githubusercontent.com/mtvguru/vax-list/main/docs/vax-list.json',
   //'https://mtv.guru/vax-list/vax-list.json'
-  'https://ftm.guru/tokenlists/abcdefx.json'
+  //'https://ftm.guru/tokenlists/abcdefx.json',
+  'https://ftm1337.eliteness.network/tokenlists/abcdefx.json'
 ]
